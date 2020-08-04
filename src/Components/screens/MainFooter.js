ef71d875b8f5bd0afa52512eca1113313dbef5ef
sts2003 <sts566311@gmail.com>
2020-08-04 21:37:29 +0900

    footer complete

--- src/Components/screens/MainFooter.js
+++ src/Components/screens/MainFooter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../styles/mainFooter.css"
 
 class MainFooter extends React.Component {
   render() {
--- src/Components/screens/MainPage.js
+++ src/Components/screens/MainPage.js
@@ -6,6 +6,10 @@ class MainPage extends React.Component {
     return (
       <div className="mainPage">
         <div className="main__bg"></div>
+        <div className="comInfo"></div>
+        <div className="priceInfo"> </div>
+        <div className="process"></div>
+        <div className="asking"></div>
       </div>
     );
   }
