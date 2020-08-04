56a16bdc652527b4faca4e0e176dc20698e2b19b
sts2003 <sts566311@gmail.com>
2020-08-04 22:00:10 +0900

    main doing

--- src/Components/screens/MainPage.js
+++ src/Components/screens/MainPage.js
@@ -6,10 +6,22 @@ class MainPage extends React.Component {
     return (
       <div className="mainPage">
         <div className="main__bg"></div>
-        <div className="comInfo"></div>
-        <div className="priceInfo"> </div>
-        <div className="process"></div>
-        <div className="asking"></div>
+        <div className="comInfo">
+          <div className="comInfo__img"></div>
+          <div className="comInfo__desc"></div>
+        </div>
+        <div className="priceInfo">
+        <div className="priceInfo__img"></div>
+          <div className="priceInfo__desc"></div>
+        </div>
+        <div className="process">
+        <div className="process__img"></div>
+          <div className="process__desc"></div>
+        </div>
+        <div className="asking">
+        <div className="asking__img"></div>
+          <div className="asking__desc"></div>
+        </div>
       </div>
     );
   }
